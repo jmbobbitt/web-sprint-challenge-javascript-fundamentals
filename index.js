8ecace3231edf7991fc907ba23b04ccaeaac8580
Jenni B <71854518+jmbobbitt@users.noreply.github.com>
2021-01-24 21:12:26 -0500

    topic 4 is complete

--- index.js
+++ index.js
@@ -212,14 +212,30 @@ console.log(cuboid.surfaceArea()); // 130
 //🦄🦄🦄 1. Take your prototypes from above and refactor into class syntax. Please rename your class CuboidMakerTwo and your object cuboidTwo 🦄🦄🦄
 class CuboidMakerTwo{
 
-  
+  constructor(obj){
+    this.length = obj.length;
+    this.width = obj.width;
+    this.height = obj.height;
+  }
 
+  surfaceArea() {
+    return 2 *(this.length*this.width + this.length*this.height + this.width*this.height);
+  }
+  volume(){
+    return this.length*this.width*this.height;
+  }
 }
+
+const cuboidTwo = new CuboidMakerTwo({
+  length:4,
+  width:5, 
+  height:5, 
+});
 
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-// console.log(cuboidTwo.volume()); // 100
-// console.log(cuboidTwo.surfaceArea()); // 130
+console.log(cuboidTwo.volume()); // 100
+console.log(cuboidTwo.surfaceArea()); // 130
 
 
 
